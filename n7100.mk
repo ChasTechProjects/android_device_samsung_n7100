0fc55147ba470536e973d0ece77cbf38bd37d9f2
ComicoTeam <comico_work@outlook.com>
2020-02-25 23:07:44 +0200

    Fix sound

--- n7100.mk
+++ n7100.mk
@@ -32,7 +32,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/t03g
+    $(LOCAL_PATH)/configs/tiny_hw.xml:system/etc/sound/n7100
 
 # Camera
 PRODUCT_PACKAGES += \
